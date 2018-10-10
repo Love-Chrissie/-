e9fb31e122e4eafc720cbdb183ab2c3111e90669
Love-Chrissie <389641222@qq.com>
2018-10-10 12:36:41 +0800

    2

---
+++ LU.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+# 输入系数矩阵应以numpy中的array类型输入
+# LU分解又称doolittle分解
+def doolittle(a, n):
+    # 主行
+    l = np.eye(n)
+    u = np.zeros((n, n))
+    for i in range(n):
+        for j in range(i + 1, n):
+            # 乘子，用于消去第j行的i列
+            a[j, i] = a[j, i] / a[i, i]
+            # 对i+1...n-1列
+            for k in range(i + 1, n):
+                a[j, k] = a[j, k] - a[j, i] * a[i, k]  # 消元
+    for i in range(n):
+        for j in range(n):
+            if (j < i):
+                l[i, j] = a[i, j]  # 下三角矩阵
+            else:
+                u[i, j] = a[i, j]  # 上三角矩阵
+    return a,l,u
---
+++ gelimm.c
@@ -0,0 +1,27 @@
+#define DIM 5
+#include "GaussElimination.c"
+extern int gelim( int process, double A[DIM][DIM], double b[DIM]);
+int main(int argc, char const *argv[])
+{
+    int i,j;
+    static double A[DIM][DIM] = {
+        {3.0, -2.0, 5.3, -2.1, 1.0},
+        {1.0, 4.0, -6.0, 4.5, -6.0},
+        {3.0, 6.0, -7.3, -9.0, 3.4},
+        {-2.0, -3.0, 1.0, -4.0, 6.0},
+        {1.0, -4.0, 6.5, 1.0, -3.0}
+    };
+    static double b[DIM] = {28.3, -36.2, 24.5, 16.2, 4.3};
+    system("cls");
+    if( gelim(1, A, b) == 1 ){
+        printf(" The linear system hasn't solution!\n");
+        printf(" Strike any key to exit!\n");
+        getch();
+        exit(1);
+    }
+    printf("Gauss elimination method:\n");
+    for(i=0; i<DIM; i++)
+        printf("   %12.6f\n", b[i]);
+    getch();
+    return 0;
+}
